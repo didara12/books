748ddc51cade77735d931fc66b1e33ebf740a6f2
didara12 <ayoubover19@gmail.com>
2024-03-08 10:45:48 +0100

    3/8/24

--- frontend/vite-project/vite.config.js
+++ frontend/vite-project/vite.config.js
@@ -4,11 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: '0.0.0.0'
-    // host: '192.168.0.173'
-    // host: 'localhost'
-    // allowedHosts: ['192.168.0.173', 'localhost']
-  },
+  // server: {
+  //   host: '0.0.0.0'
+  //   // host: '192.168.0.173'
+  //   // host: 'localhost'
+  //   // allowedHosts: ['192.168.0.173', 'localhost']
+  // },
 
 })
